69ec88e96a4d29575357c59b5386c6e0ff372b2a
camancero <sebastianmancero@hotmail.com>
2017-03-17 14:23:38 +0000

    Update DataSet.m

--- DataSet.m
+++ DataSet.m
@@ -287,5 +287,36 @@ classdef DataSet
             X = [Y{z};X];
             end
         end
+        
+        %% Windows
+        %Gaussian kernel
+        function f = gaussk(x, mu, s)
+            ex = (-1/2)*((x - mu)/s).^2;
+            ar = (s * sqrt(2*pi));
+            f = (1/ar)*exp(ex);
+        end
+        %Exponential kernel
+        function f = expk(x, mu, s)
+            ex = (-sqrt(2))*abs((x-mu)/s);
+            ar = (s * sqrt(2));
+            f = (1/ar)*exp(ex);
+        end
+        %Causal kernel
+        function hw = cauk(x,mu,a)
+            q = ((a^2)*(x-mu));
+            w = exp((-a).*(x-mu));
+            for i = 1:length(q)
+                f(i)=q(i)*w(i);
+            end
+            hw = zeros(size(f));
+            for i = 1:length(f)
+                if f(i) > 0.0
+                    hw(i) = f(i);
+                else
+                    hw(i) = 0.0;
+                end
+            end
+        end
+
     end
 end
